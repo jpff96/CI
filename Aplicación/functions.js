eb5d61837c5a69738380d24cd747ae5bb08311b1
Juan Pablo Fajardo Fontes <juan.fajardo@ibm.com>
2018-09-11 11:34:42 -0300

    subo front end

---
+++ Aplicación/functions.js
@@ -0,0 +1,6 @@
+function move_to_info(){
+    var home_container = document.getElementById('home_container');
+    home_container.style.display = 'none'
+    var info_container = document.getElementById('info_container');
+    info_container.style.display = 'block'
+}
